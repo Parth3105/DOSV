9f2ae1881ef99a2c3377440788b02ea3e41022fe
Desai Aditya Veeral <121945914+adi-25704@users.noreply.github.com>
2025-04-08 03:03:39 +0530

    uploading SQLite connection code (#6)

--- MetaServerProgram/src/UploadHandler.java
+++ MetaServerProgram/src/UploadHandler.java
@@ -1,5 +1,7 @@
 import java.io.*;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UploadHandler implements Handler {
@@ -44,17 +46,11 @@ public class UploadHandler implements Handler {
             }
 
             // Retrieve file.
-            FileOutputStream fileWriter = new FileOutputStream(path); // write into file.
             ObjectInputStream clientObj=new ObjectInputStream(socket.getInputStream());
             List<byte[]> chunks= (List<byte[]>) clientObj.readObject();
-
-            // Write the file.
-            for(byte[] chunk: chunks){
-                fileWriter.write(chunk);
-                fileWriter.flush();
-            }
-            fileWriter.close();
             System.out.println("File Received Successfully!!!!"); //debug
+            SendToSQLiteDB(chunks, fileName); // send to DB.
+
         } catch (IOException e) {
             // handle error
             throw new RuntimeException(e);
@@ -63,7 +59,72 @@ public class UploadHandler implements Handler {
         }
 
     }
+    public void SendToSQLiteDB(List<byte[]> chunks, String fileName) {
+        String serverIP = "192.168.110.94";
+        int serverPort = 8090;
+        int maxRetries = 3; 
+        int retryDelay = 2000; 
+        
+        Socket socket = null;
+        int attempt = 0;
 
+        DataOutputStream dataOutputStream=null;
+        while (attempt < maxRetries) {
+            try {
+                socket = new Socket(serverIP, serverPort);
+                System.out.println("Connected to Database at " + serverIP + ":" + serverPort);
+                dataOutputStream=new DataOutputStream(socket.getOutputStream());
+                break; // Successful connection, exit loop
+            } catch (ConnectException ce) {
+                // System.err.println("Attempt " + (attempt + 1) + ": Could not connect to server. Retrying...");
+            } catch (IOException e) {
+                // System.err.println("Attempt " + (attempt + 1) + ": Failed to establish connection. Retrying...");
+            }
+
+            attempt++;
+            if (attempt < maxRetries) {
+                try {
+                    // Wait before retrying
+                    Thread.sleep(retryDelay); 
+                } catch (InterruptedException ie) {
+                    System.err.println("Database Connection interrupted. Exiting.");
+                    System.exit(1);
+                }
+            } else {
+                System.err.println("Could not connect to Database! Database might be down.");
+                System.exit(1);
+            }
+        }
+         try {
+            // Send write-request and filename
+            dataOutputStream.writeUTF("STORE");
+            dataOutputStream.flush();
+            //Saving of object in a file
+            String fileType = fileName.substring(fileName.lastIndexOf('.') + 1); // Extract file type
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+            List<String> chunk_names = new ArrayList();
+            for (int i = 0; i < chunks.size(); i++) {
+                chunk_names.add(fileName+"_chunk_"+ i+"." + fileType); // create chunk names.
+            }
+            System.out.println("Sending chunk names to DB"); //debug
+            out.writeObject(chunk_names); // write the names of the chunks to the file.
+            out.flush();
+            // Method for serialization of object
+            System.out.println("Sending chunks to DB"); //debug
+            out.writeObject(chunks);
+            out.flush();
+            System.out.println("Chunks sent to DB"); //debug
+            out.close();
+            
+        } catch (IOException e) {
+            System.err.println("Error while sending write request or filename: " + e.getMessage());
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            System.err.println("DataOutputStream or fileName is null: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
+    }
     @Override
     public void sendResponse() {
 
